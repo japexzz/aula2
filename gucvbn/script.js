6c4674ec5f288ea1816eec8c8993bf1f86f6ae83
japexzz <138229496+japexzz@users.noreply.github.com>
2023-10-06 11:58:04 -0300

    Add files via upload

---
+++ gucvbn/script.js
@@ -0,0 +1,22 @@
+function pedir(){
+var opcao=prompt("escolha uma opcão de 1 a 4")
+
+switch(Number(opcao)){
+    case 1:
+        alert("você escolheu coca cola")
+        break;
+
+        case 2:
+            alert("você escolheu macarrão")
+            break;
+
+            case 3:
+                alert("você ecolheu coxinha")
+                break;
+                
+                case 4:
+                    alert("você escolheu sorvete")
+                    break;
+}
+
+}
